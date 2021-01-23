6ac90c96ab7883c0c6dcfe40be731d2f3bbeb2e9
suncerrae <sun.quinones@bryanuniverrsity.edu>
2021-01-23 11:32:23 -0500

    x

---
+++ week1/math/app.js
@@ -0,0 +1,13 @@
+const math = require("./math.js");
+
+console.log(math.add(20, 30));
+console.log(math.subtract(60, 5));
+console.log(math.multiply(5, 15));
+console.log(math.divide(100, 20));
+
+
+
+
+
+
+
